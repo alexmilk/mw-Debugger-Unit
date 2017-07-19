6567d2f2d3b622fba160c7a57e4f04666c314529
Alex <alex.milkis@kaltura.com>
2017-07-19 22:26:46 +0300

    updated mainApp.js

--- Debugging-unit/mainApp.js
+++ Debugging-unit/mainApp.js
@@ -1,9 +1,7 @@
-/**
- * Created by alex.milkis on 09/07/2017.
- */
+const playerFrame = document.kPlayer_ifp_ifp;
 //kBinds and other functions
 kWidget.addReadyCallback(function (playerId) {
-    var kdp = document.getElementById(playerId);
+    let kdp = document.getElementById(playerId);
     kdp.kBind('playerReady', function () {
         $("#analyticsLog").append(kdp.evaluate("Player Version: " + "{playerVersion}<br>\n")).addClass("greenText");
         $("#analyticsLog").append(kdp.evaluate("Entry Id: " + "{mediaProxy.entry.id}\n<br>")).addClass("greenText");
@@ -12,8 +10,8 @@ kWidget.addReadyCallback(function (playerId) {
         });
         $("#entrydit").replaceWith(kdp.evaluate("<b>Entry id: </b>" + "<b>{mediaProxy.entry.id}\n</b>"));
         $("#uiconf").replaceWith(kdp.evaluate("<b>Player id: </b>" + "<b>{configProxy.kw.uiConfId}\n</b>"));
-        $.loadedTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
-        $("#loadTime").append("Page loaded in " + $.loadedTime + " milliseconds");
+        $.loadedTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+        $("#loadTime").prepend("Page loaded in " + $.loadedTime + " milliseconds");
     });
 
     kdp.kBind('mediaReady', function () {
@@ -34,8 +32,17 @@ kWidget.addReadyCallback(function (playerId) {
     kdp.kBind('doPlay', function () {
         $("#analyticsLog").append("Player Playing\n<br>").addClass("greenText");
     });
-    kdp.kBind('mediaError', function () {
-        $("#analyticsLog").append("Media Error\n<br>").addClass("greenText");
+    kdp.kBind('playerError', function (errorEvent) {
+        $("#errorBar").slideDown(300);
+        $("#errorBar p").append(" - " + errorEvent.message);
+    });
+    kdp.kBind('mediaErrorError', function (errorEvent) {
+        $("#errorBar").slideDown(300);
+        $("#errorBar p").append(" - " + errorEvent.message);
+    });
+    //OK button handler
+    $('#okError').click(function () {
+        $('#errorBar').slideUp(300);
     });
     kdp.kBind('bumperStarted', function () {
         $("#analyticsLog").append("Bumper Started\n<br>").addClass("greenText");
@@ -48,105 +55,171 @@ kWidget.addReadyCallback(function (playerId) {
 //The mainBrain
 function setEntry() {
     kWidget.addReadyCallback(function (playerId) {
-        var kdp = document.getElementById(playerId);
-        var input = document.getElementById("adInput").value;
-        kdp.sendNotification("changeMedia", {"entryId": input});
+        let kdp = document.getElementById(playerId);
+        let input = document.getElementById("adInput").value;
+        kdp.sendNotification("changeMedia", {
+            "entryId": input
+        });
     })
 }
 $("#cleanMedia").click(function () {
     kWidget.addReadyCallback(function (playerId) {
-        var kdp = document.getElementById(playerId);
+        let kdp = document.getElementById(playerId);
         kdp.sendNotification("cleanMedia");
     })
-})
+});
+
 function setKS() {
     kWidget.addReadyCallback(function (playerId) {
-        var kdp = document.getElementById(playerId);
-        var input = document.getElementById("ksInput").value;
+        let kdp = document.getElementById(playerId);
+        let input = document.getElementById("ksInput").value;
         kdp.setKDPAttribute('servicesProxy.kalturaClient', 'ks', input);
     })
 }
+
 function doPlay() {
     kWidget.addReadyCallback(function (playerId) {
-        var kdp = document.getElementById(playerId);
+        let kdp = document.getElementById(playerId);
         kdp.sendNotification("doPlay");
     })
 }
+
 function doStop() {
     kWidget.addReadyCallback(function (playerId) {
-        var kdp = document.getElementById(playerId);
+        let kdp = document.getElementById(playerId);
         kdp.sendNotification("doStop");
     })
 }
+
 function callAd() {
     kWidget.addReadyCallback(function (playerId) {
-        $.kPlayer = document.kPlayer_ifp_ifp;
-        window.open($.kPlayer.kalturaIframePackageData.playerConfig.plugins.doubleClick.adTagUrl);
+        let adInput = $('#adTagId').val();
+        window.open(adInput);
         return false;
-    })}
-$( document ).ready(function (){
-    var msg = $.ajax({type: "GET", url: "https://tinyurl.com/api-create.php?url=" + document.location.href, async: false}).responseText;
-    $("#tinyUrl").append(msg);
-    return false;
-});
+    })
+}
 
+function changeEmbed() {
+    if ($('#customEmbedParams').css("display") == "none") {
+        $("#customEmbedParams").slideDown(500);
+    }
+    else {
+        $("#customEmbedParams").slideUp(500);
+    }
+}
 kWidget.addReadyCallback(function (playerId) {
-    var kdp = document.getElementById(playerId);
+    let kdp = document.getElementById(playerId);
     kdp.kBind('playerReady', function () {
         $.kPlayer = document.kPlayer_ifp_ifp;
-        $("#appendHtml").append(JSON.stringify(document.kPlayer_ifp_ifp.kalturaIframePackageData.playerConfig.plugins, null, 2));
+        $("#appendJSON").append(JSON.stringify(document.kPlayer_ifp_ifp.kalturaIframePackageData.playerConfig.plugins, null, 2));
         $(".mw-debug-info").append(document.kPlayer_ifp_ifp.kalturaIframePackageData.playerConfig.plugins.debugInfo);
         return false;
-    })});
-$( document ).ready(function() {
-    kWidget.addReadyCallback(function (playerId) {
-        $.kPlayer = document.kPlayer_ifp_ifp;
-        var input = $("#adTagId");
-        input.val( input.val() + $.kPlayer.kalturaIframePackageData.playerConfig.plugins.doubleClick.adTagUrl);
-        return false;
-    })});
+    })
+});
+//        $(document).ready(function () {
+//            kWidget.addReadyCallback(function (playerId) {
+//                $.kPlayer = document.kPlayer_ifp_ifp;
+//                const input = $("#adTagId");
+//                input.val(input.val() + $.kPlayer.kalturaIframePackageData.playerConfig.plugins.doubleClick.adTagUrl);
+//                return false;
+//            })
+//        });
 // Top menu buttons and logic
-$("#removeThumb").click(function() {
-    var kPlayerContent = $("#kPlayer_ifp_ifp").contents()[0];
-    $(kPlayerContent).find("#kPlayer_ifp").css("visibility", "hidden");
+$("#removeThumb").click(function () {
+    const playerContent = $("#kPlayer_ifp_ifp").contents()[0];
+    const playerThumb = $(playerContent).find("#kPlayer_ifp");
+    if ($(playerThumb).css("visibility") == 'visible') {
+        document.getElementById('removeThumb').textContent = "Show thumbnail";
+        $(playerThumb).css("visibility", "hidden");
+    }
+    else {
+        $(playerThumb).css("visibility", "visible");
+        document.getElementById('removeThumb').textContent = "Hide thumbnail";
+    }
 });
-$("#removePlay").click(function(){
-    var playButton = $("#kPlayer_ifp_ifp").contents()[0];
-    $(playButton).find(".largePlayBtn").css("visibility", "hidden");
+$("#removePlay").click(function () {
+    const playerContent = $("#kPlayer_ifp_ifp").contents()[0];
+    const playButton = $(playerContent).find(".largePlayBtn");
+    if ($(playButton).css("display") == 'block') {
+        document.getElementById('removePlay').textContent = "Show play button";
+        $(playButton).css("display", "none");
+    }
+    else {
+        $(playButton).css("display", "block");
+        document.getElementById('removePlay').textContent = "Hide play button";
+    }
 });
-$("#hideControls").click(function() {
+$("#hideControls").click(function () {
     const hideControls = $("#kPlayer_ifp_ifp").contents()[0];
     const controls = window.kPlayer_ifp_ifp.document.querySelector('.controlBarContainer');
     if (controls.style.display == "none") {
         controls.style.display = "block";
         document.getElementById('hideControls').textContent = "Hide controls bar";
-    } else {
+    }
+    else {
         controls.style.display = "none";
         document.getElementById('hideControls').textContent = "Show controls bar";
     }
 
 });
-function goToVast() {
-    let k = document.kPlayer_ifp_ifp;
-    let vastWindow = window.open("https://developers.google.com/interactive-media-ads/docs/sdks/html5/vastinspector?tag=" + k.kalturaIframePackageData.playerConfig.plugins.doubleClick.adTagUrl);
-    $(vastWindow).ready(function() {
-        document.getElementById('test-ad-button').click();
-    })};
 
-$("#getSource").click(function (){
-    var playerSources = document.kPlayer_ifp_ifp.kPlayer_ifp.getSources();
-    for (var i = 0; i < playerSources.length; i++) {
+function goToVast() {
+    window.open("https://developers.google.com/interactive-media-ads/docs/sdks/html5/vastinspector?tag=" + document.kPlayer_ifp_ifp.kalturaIframePackageData.playerConfig.plugins.doubleClick.adTagUrl);
+}
+
+function getActiveSource() {
+    const playerSource = document.kPlayer_ifp_ifp.kPlayer_ifp;
+    const currentSource = playerSource.getSource();
+    window.location.assign(currentSource.src);
+}
+
+function getActiveBitrate() {
+    let k = document.kPlayer_ifp_ifp;
+    let k2 = k.document.getElementsByClassName('persistentNativePlayer nativeEmbedPlayerPid');
+    let sumk = k2.pid_kPlayer_ifp.webkitVideoDecodedByteCount / 1000;
+    alert(sumk);
+}
+
+function downloadJSON() {
+    let jsonContent = document.getElementById('appendJSON');
+    window.open('_blank').document.write(jsonContent.innerHTML);
+}
+$("#getSource").click(function () {
+    let playerSources = document.kPlayer_ifp_ifp.kPlayer_ifp.getSources();
+    for (let i = 0; i < playerSources.length; i++) {
         parent.document.getElementById('appendStats').append(JSON.stringify(playerSources[i], null, 2));
     }
 });
-
-$("#getCaptions").click(function (){
-    var playerCaptions = document.kPlayer_ifp_ifp.kPlayer_ifp.mediaElement.getTextTracks();
-    for (var i = 0; i < playerCaptions.length; i++) {
+$("#getCaptions").click(function () {
+    let playerCaptions = document.kPlayer_ifp_ifp.kPlayer_ifp.mediaElement.getTextTracks();
+    for (let i = 0; i < playerCaptions.length; i++) {
         parent.document.getElementById('appendStats').append(JSON.stringify(playerCaptions[i], null, 2));
     }
 });
-$('button[type=button], input[type=submit]').hover(function() {
-    $( this ).fadeOut( 50 );
-    $( this ).fadeIn( 50 );
+$('button[type=button], input[type=submit]').hover(function () {
+    $(this).fadeOut(50);
+    $(this).fadeIn(50);
 });
+$(document).ready(function () {
+    $('#topHeaderDiv, #appendStats, #kPlayer_ifp, #mainControlDiv').fadeIn(400);
+});
+$(document).ready(function() {
+    const adSlots = {
+        inlinePreroll: {
+            "slotType": "Inline preroll",
+            "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=",
+        },
+        skippablePreroll: {
+            "slotType": "skippable preroll",
+            "adTagUrl": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=",
+        }
+    };
+    for (let prop in adSlots) {
+        let str = "lalalala";
+        $("#adSlotSelector").append( "<option value='new'>" + adSlots[prop].slotType + "</option>" )
+        //adSlots[prop].slotType
+    }
+});
+let tiny = function(){
+    window.open("http://tinyurl.com/api-create.php?url="+$(location).attr('href')+$(location).attr('search'), '_blank');
+}
