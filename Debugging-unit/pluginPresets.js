0553508dba3412ae10ba12d9b29ef30ed5958ded
Alex <alex.milkis@kaltura.com>
2017-08-10 12:22:00 +0300

    Changed embed params sections

--- Debugging-unit/pluginPresets.js
+++ Debugging-unit/pluginPresets.js
@@ -3,17 +3,17 @@
  */
 var plugins = [
     {
-        "displayName" : "vast",
+        "displayName" : "DoubleClick With Debugging",
         "json" : {
-            "vast":{
-                "prerollUrl":"http://projects.kaltura.com/vast.xml",
-                "preSequence":1,
-                "numPreroll":1,
-                "skipOffset":5,
-                "storeSession":false,
-                "timeout":4,
-                "plugin":true
-            }
+            "DoubleClick": {
+                "plugin": true,
+                "adTagUrl": "",
+                "prerollUrlJs": null,
+                "leadWithFlash": true,
+                "trackCuePoints": false,
+                "debugMode": true, //Should enable debugMode for DoubleClick systems.
+                "adsManagerLoadedTimeout": 15000
+            },
         }
     } ,
     {
@@ -86,36 +86,3 @@ var plugins = [
         }
     }
 ]
-var pluginsCombo=$('<div class="dropdown btn-group"><a class="btn dropdown-toggle" data-toggle="dropdown" href="#">Add plugin<span class="caret"></span></a><ul id="dropDownContent" class="dropdown-menu"></ul></div>');
-$("#pluginsContainer").append(pluginsCombo);
-
-$(plugins).each(function(){
-    var liItem = $('<li><a href="#">'+this.displayName+'</a></li>').data(this.json).attr("pname",this.displayName);
-    $("#dropDownContent").append(liItem);
-})
-
-function insertAt(src, index, str) {
-    return src.substr(0, index) + str + src.substr(index)
-}
-
-$("#dropDownContent").delegate("li", "click", function (e) {
-    //alert( JSON.stringify($(this).data(), null, 2)  );
-    if($("#jsonTextArea").val().indexOf("{") == 0){
-        // check if the current JSON has this specific plugin
-        if($("#jsonTextArea").val().indexOf( $(this).attr("pname") ) > -1 ){
-            //This plugin exists in the JSON. Alert the user
-            alert("Error. It looks like this plugin already exists in the your JSON config ");
-        } else {
-            //add the plugin into the JSON
-            var shortJSON = JSON.stringify($(this).data(), null, 2);
-            shortJSON = shortJSON.slice( 1 ); // remove first {
-            shortJSON = shortJSON.slice(0, -1); //remove last }
-            shortJSON = shortJSON.slice(0, -1); //remove lin break
-            shortJSON = shortJSON + ","; // add comma, this will not be the 1st plugin
-            $("#jsonTextArea").val( insertAt($("#jsonTextArea").val() , 1 , shortJSON));
-        }
-    }else{
-        //this is the 1st plugin that we are adding
-        $("#jsonTextArea").val(JSON.stringify($(this).data(), null, 2)) ;
-    }
-});
